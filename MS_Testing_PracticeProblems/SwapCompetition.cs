139a2f2034a384cfc18f85185bf99e35a4e398ab
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-26 08:08:48 +0530

    [somu]Add:Swap condition MS-Unit Testing.

---
+++ MS_Testing_PracticeProblems/SwapCompetition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_Testing_PracticeProblems
+{
+    public class SwapCompetition
+    {
+        public string SwapCondition(string one, string two)
+        {
+            List<string> list1 = new List<string>();
+            list1.Sort();
+            List<string> list2= new List<string>();
+            list2.Sort();
+            string word1=string.Join("",list1);
+            string word2=string.Join("",list2);
+            if(word1.Equals(word2))
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
+        }
+    }
+}
--- PracticeProblemTestProject/UnitTest1.cs
+++ PracticeProblemTestProject/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace PracticeProblemTestProject
     public class UnitTest1
     {
         [TestMethod]
+        //Purchase List
         public void ShopKeeperSoldItem_WhenAnalyze_ShouldReturnSoldOnce()
         {
             int[] prices = { 10, 20, 30, 10, 40, 50, 20 };
@@ -20,6 +21,21 @@ namespace PracticeProblemTestProject
             PurchaseListOperation purchase = new PurchaseListOperation();
             int result = purchase.SoldMore(prices);
             Assert.AreEqual(result,10);
+        }
+        [TestMethod]
+        // Swap condition
+        public void SwapCondition_WhenAnalyzeNoonMoon_ShouldReturn_No()
+        {
+            SwapCompetition swap=new SwapCompetition();
+            string result = swap.SwapCondition("noon", "moon");
+            Assert.AreEqual("NO", result);
+        }
+        [TestMethod]
+        public void SwapCondition_WhenAnalyzePartTrap_ShouldReturn_Yes()
+        {
+            SwapCompetition swap = new SwapCompetition();
+            string result = swap.SwapCondition("part", "trap");
+            Assert.AreEqual("YES", result);
         }
     }
 }
